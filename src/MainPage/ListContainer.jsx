8bf16979f5a2f9676c1eefe54262041e87c3b80d
Davitamane <datokhizanishvili005@gmail.com>
2025-10-04 15:32:29 +0400

    added vercel configs

--- src/MainPage/ListContainer.jsx
+++ src/MainPage/ListContainer.jsx
@@ -33,7 +33,6 @@ function ListContainer() {
     return matchingRegion && matchingPrice && matchingArea && matchingBedrooms;
   });
 
-  // console.log(filteredTasks);
 
   return (
     <div className="w-full grid my-10 gap-4 grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
